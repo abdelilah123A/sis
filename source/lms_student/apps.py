6f1a83ca3ebda8d8cfe1ed9b80ed407275a5dbcc
A.KAHLAOUI <professeurkahlaoui@gmail.com>
2023-04-12 08:48:46 +0000

    Add project Apps

---
+++ source/lms_student/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LmsStudentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lms_student'
---
+++ source/payment_student/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PaymentStudentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'payment_student'

a65a388005ffc7725378e16114c3cd95eaf64be5
abdelilah123A <a.elgharbaoui@mundiapolis.ma>
2023-04-26 09:27:49 +0100

    first

---
+++ my_env/source/pointage/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Class_session,Attendance
+
+
+
+# Register your models here.
+admin.site.register(Class_session)
+admin.site.register(Attendance)
---
+++ my_env/source/pointage/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+from lms_teacher import Teacher
+from lms_student import Course,Student
+
+# Create your models here.
+class Class_session(models.Model):
+    session_id=models.PrimaryKey(
+        max_length=50,
+        null=False,
+        on_delete=models.SET_NULL,
+       
+       )
+    session_date = models.DateField(
+          max_length=50
+    
+        )
+    start_time= models.TimeField(
+          max_length=50
+    
+        )
+    end_time = models.TimeField(
+          max_length=50
+    
+        )
+    status= models.CharField(
+              
+        max_length=50,
+        null=True,
+        default='planned',
+        verbose_name='Status',
+       
+        
+    
+        )
+    comment= models.CharField(
+          max_length=50
+    
+
+        )
+    teacher_id=models.ForeignKey(
+        Teacher,
+        max_length=50,
+        null=True,
+        verbose_name='teacherId',
+        on_delete=models.SET_NULL,
+        
+    )
+    course_id=models.ForeignKey(
+        Course,
+        max_length=50,
+        null=True,
+       
+        verbose_name='courseId',
+        on_delete=models.SET_NULL,
+       
+    )
+class Attendance(models.Model):
+    session_id = models.ForeignKey(
+        Class_session,
+    )
+    student_id = models.ForeignKey(
+       Student,
+       )
+    #primary_key = models.CompositeField(fields=['session_id', 'student_id'], primary_key=True)
+   
+    class Meta:
+       unique_together = ('session_id','student_id')
